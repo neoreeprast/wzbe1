f69617f2dacd8ef88bf23a415c9333c07b80a5dd
Arie <arie.prastowo@gmail.com>
2023-11-11 22:51:15 +0700

    Add jwttokenfilter to check authentication token

--- src/main/java/com/rumanira/waizly/wzbe1/config/SecurityConfig.java
+++ src/main/java/com/rumanira/waizly/wzbe1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.rumanira.waizly.wzbe1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,19 +20,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
         http
         .csrf((requests) -> requests.disable())
+        .sessionManagement((sm) -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests((requests) -> requests
         .requestMatchers("auth/login").permitAll()
         .requestMatchers("employees/**").hasAnyRole("HR_ADMIN", "SALES_MANAGER")
         .requestMatchers("sales/**").hasAnyRole("SALES_MANAGER")
-        .anyRequest().authenticated());
+        .anyRequest().authenticated())
+        .addFilterBefore(new JwtTokenFilter(jwtTokenProvider, userDetailsService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
